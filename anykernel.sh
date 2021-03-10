c6fd40ae9ec116fee0cda9e39537a91c446261d9
Muhammad Fitriyan <72380503+Xzyann1411@users.noreply.github.com>
2021-03-11 02:09:47 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ryzen Kernel by Edwiin Kusuma Jaya @ telegram
+kernel.string=Predator Kernel by MhdIyan
 do.devicecheck=1
 do.modules=1
 do.systemless=0
