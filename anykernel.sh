66d9ce07495e274e852e076612416f5c14b0b074
Muhammad Fitriyan <72380503+Xzyannn@users.noreply.github.com>
2021-08-13 10:25:37 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Predator Kernel by MhdIyan
+kernel.string=Predator reborn Kernel by Xzyannn
 do.devicecheck=1
 do.modules=1
 do.systemless=0
